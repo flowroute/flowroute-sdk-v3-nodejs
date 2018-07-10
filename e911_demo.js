73e532c7dcc0e9fdaa2de0720404b713db660893
Maria Bermudez <bermudez.mt@gmail.com>
2018-07-09 22:11:11 -0700

    Add E911 endpoints demo

--- e911_demo.js
+++ e911_demo.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 require('dotenv').config();
 const flowroute = require('./lib');
+var callback = function(error, response, context){}
 
 console.log("Number//Route Management v2 & Messaging v2.1 Demo");
 
@@ -13,25 +14,33 @@ mobile_number = process.env.MOBILE_NUMBER
 var e911s_controller = flowroute.E911sController;
 var numbers_controller = flowroute.NumbersController;
 
-
-//var e911address = new E911Address({"label": "First e911 record"});
-
-
-var callback = function(error, response, context){}
-/*
+var e911address = {"data":{"type": "e911", "attributes":{"label": "First e911 record", "first_name": "Bob", "last_name": "Law", "street_number": "123", "street_name": "Smith St", "city": "Seattle", "state": "WA", "country": "US", "zip": "98101"}}};
 e911Address = e911s_controller.createAndValidateANewE911Address(e911address, callback);
-areacodes.then(function(response) {
+e911Address.then(function(response) {
   console.log("--Create and Validate a New E911 Address")
   console.log(response);
 }, function(err) {
   console.log(err);
 });
-*/
 
-e911_addresses = e911s_controller.listAccountE911Addresses(limit=3, callback);
-e911_addresses.then(function(response) {
+
+//List E911 Addresses on Account
+
+var e911_result = "";
+var e911_id = null;
+var e911_addresses = e911s_controller.listAccountE911Addresses(limit=3, callback);
+e911_addresses.then(async (response) => {
+  e911_result = response;
+  e911_id = e911_result['data'][0]['id'];
+
   console.log("--List Account E911 Addresses")
-  console.log(response);
+  console.log(JSON.stringify(e911_result, null, 2));
+
+  console.log("--List E911 Record Details")
+  var e911_address_details = await e911s_controller.listE911RecordDetails(e911_id, callback);
+  console.log(JSON.stringify(e911_address_details, null, 2));
+
 }, function(err) {
   console.log(err);
 });
+
