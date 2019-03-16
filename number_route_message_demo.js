d9f80fc242c6369ec9471d468446f1c18556a3c2
Denise Mauldin <dmauldin@flowroute.com>
2019-03-16 10:02:27 -0400

    Update numbers demo.

--- number_route_message_demo.js
+++ number_route_message_demo.js
@@ -12,106 +12,93 @@ mobile_number = process.env.MOBILE_NUMBER;
 // Instantiate API client and create controllers for Numbers, Messages, and Routes
 const numbers_controller = flowroute.NumbersController;
 const routes_controller = flowroute.RoutesController;
-const messages_controller = flowroute.MessagesController;
+const messagesController = flowroute.MessagesController;
 
+var finished = false;
 const max_setup_cost = 3.25;
 const limit = 3;
 const offset = 0;
 
-var callback = function(error, response, context){}
+var callback = function (error, response, context) { }
 
 let areacodes = numbers_controller.listAvailableAreaCodes(limit, offset, max_setup_cost, callback);
-areacodes.then(function(response) {
+areacodes.then(async function (response) {
   console.log("--List Available Area Codes")
   console.log(response);
-}, function(err) {
-  console.log(err);
-});
 
-var areacode = 206;
-excodes = numbers_controller.listAvailableExchangeCodes(limit, offset, max_setup_cost, areacode, callback);
-
-excodes.then(function(response) {
+  var areacode = 206;
+  excodes = await numbers_controller.listAvailableExchangeCodes(limit, offset, max_setup_cost, areacode, callback);
   console.log("--List Available Exchange Codes")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
+  console.log(excodes);
 
-var starts_with = 1206
-var contains = 3
-var ends_with = 7
-var rate_center = "Seattle"
-var state = "WA"
-ppnumbers = numbers_controller.searchForPurchasablePhoneNumbers(starts_with, contains, ends_with, limit, offset, rate_center, state, callback)
-ppnumbers.then(function(response) {
+  var starts_with = 1206
+  var contains = 3
+  var ends_with = 7
+  var rate_center = "Seattle"
+  var state = "WA"
+  ppnumbers = await numbers_controller.searchForPurchasablePhoneNumbers(starts_with, contains, ends_with, limit, offset, rate_center, state, callback)
   console.log("--Search for Purchasable Phone Numbers")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
+  console.log(ppnumbers);
 
-var numberID = 12065014377
-/*purchasenumber = numbers_controller.purchaseAPhoneNumber(numberID, callback)
-purchasenumber.then(function(response) {
-  console.log("--Purchase a Phone Number")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});*/
-
-accountnumbers = numbers_controller.getAccountPhoneNumbers(callback)
-accountnumbers.then(function(response) {
+  accountnumbers = await numbers_controller.getAccountPhoneNumbers(callback)
   console.log("--List Account Phone Numbers")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
+  console.log(accountnumbers);
 
-numberdetails = numbers_controller.getPhoneNumberDetails(numberID, callback)
-numberdetails.then(function(response) {
+  var numberID = mobile_number;
+  /*numberpurchase = numbers_controller.purchaseAPhoneNumber(numberID, callback)
+  console.log("--Purchase a Phone Number")
+  console.log(numberpurchase);
+  */
+
+  numberdetails = await numbers_controller.getPhoneNumberDetails(numberID, callback)
+
   console.log("--List Phone Number Details")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
+  console.log(numberdetails);
 
-accountroutes = routes_controller.listInboundRoutes(limit, callback)
-accountroutes.then(function(response) {
+  accountroutes = await routes_controller.listInboundRoutes(limit, callback)
   console.log("--List Inbound Routes")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
+  console.log(accountroutes);
 
-var message_id = "mdr2-ca82be46e6ba11e79d08862d092cf73d";
-mdrdetailed = messages_controller.lookUpAMessageDetailRecord(message_id, callback)
-mdrdetailed.then(function(response) {
-  console.log("--Look Up a Message Detail Record")
-  console.log(response);
-}, function(err) {
-  console.log(err);
-});
-
-request_body = {
-  "data": {
-    "type": "message",
-    "attributes": {
-      "to": String(mobile_number),
-      "from": String(number_id),
-      "body": "hello there",
-      "is_mms": "true",
-      "media_urls": ["http://s3.amazonaws.com/barkpost-assets/50+GIFs/37.gif"]
+  request_body = {
+    "data": {
+      "type": "message",
+      "attributes": {
+        "to": String(mobile_number),
+        "from": String(numberID),
+        "body": "hello there",
+        "is_mms": "true",
+        "media_urls": ["http://s3.amazonaws.com/barkpost-assets/50+GIFs/37.gif"]
+      }
     }
   }
-}
-console.log("request", request_body)
+  console.log("request", request_body)
 
-result = messagesController.createSendAMessage(request_body).then((result) => {
+  result = await messagesController.createSendAMessage(request_body)
   console.log("Message result")
   console.log(result)
-},
-(error) => {
-    console.log("Message Error", error)
-})
 
+  var mdrInfo = JSON.parse(result)
+  console.log(mdrInfo)
+  var mdrId = mdrInfo['data']['id']
+  console.log("getting ", mdrId)
+  // MDR Info takes a few seconds to make it through Flowroute queue and be sent
+  setTimeout(async function () {
+    try {
+      mdrdetailed = await messagesController.lookUpAMessageDetailRecord(mdrId, callback)
+      console.log("--Look Up a Message Detail Record")
+      console.log(mdrdetailed);
+    } catch (err) {
+      console.log("error fetching mdr", err)
+    }
+    finished = true;
+  }, 10000);
+
+}, function (err) {
+  console.log(err);
+});
+
+function wait () {
+  if (!finished)
+       setTimeout(wait, 1000);
+};
+wait();
